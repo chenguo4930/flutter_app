cdb654dc6926afdcc56721fdd2d1b91b81a8c8d8
cheng <chengguo4930@hotmail.com>
2019-10-27 22:41:07 +0800

    Initial commit

---
+++ lib/card_recommend.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/base_cared.dart';
+
+///本周推荐
+class CardRecommend extends BaseCard {
+  @override
+  _CardRecommendState createState() => _CardRecommendState();
+}
+
+class _CardRecommendState extends BaseCardState {
+  @override
+  void initState() {
+    subTitleColor = Color(0xffb99444);
+    super.initState();
+  }
+
+  @override
+  bottomContent() {
+    return Expanded(
+        child: Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Image.network(
+        'http://www.devio.org/io/flutter_beauty/card_1.jpg',
+        fit: BoxFit.cover, //宽高会充满父容器，会裁切
+      ),
+    ));
+  }
+
+  @override
+  Widget topTitle(String title) {
+    return super.topTitle('本周推荐');
+  }
+
+  @override
+  Widget subTitle(String title) {
+    return super.subTitle('送你一天无限卡·全场书籍免费读 >');
+  }
+}
---
+++ lib/content_pager.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/card_recommend.dart';
+import 'package:flutter_app/custom_appbar.dart';
+
+class ContentPager extends StatefulWidget {
+  final ValueChanged<int> onPageChanged;
+  final ContentPagerController contentPagerController;
+
+//构造方法，可选参数
+  const ContentPager({Key key, this.onPageChanged, this.contentPagerController})
+      /**初始化列表**/
+      : super(key: key);
+
+  @override
+  _ContentPagerState createState() => _ContentPagerState();
+}
+
+class _ContentPagerState extends State<ContentPager> {
+  PageController _pageController = PageController(
+      /**视图比例**/
+      viewportFraction: 0.8);
+
+  static List<Color> _colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.orange,
+    Colors.teal,
+  ];
+
+  ///dart生命周期方法，创建之后，会第一时间调用
+  @override
+  void initState() {
+    if (widget.contentPagerController != null) {
+      widget.contentPagerController._pageController = _pageController;
+    }
+    _statusBar();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        //appBar
+        CustomAppBar(),
+        Expanded(
+            //高度撑开，否则在Column中没有高度会报错
+            child: PageView(
+                onPageChanged: widget.onPageChanged,
+                controller: _pageController,
+                children: <Widget>[
+              _wrapItem(CardRecommend()),
+              _wrapItem(CardRecommend()),
+              _wrapItem(CardRecommend()),
+              _wrapItem(CardRecommend())
+            ]))
+      ],
+    );
+  }
+
+  Widget _wrapItem(Widget widget) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: widget,
+    );
+  }
+
+  ///状态栏样式-沉浸式
+  _statusBar() {
+    //黑色沉浸式状态栏，基于SystemUiOverlayStyle.dark修改了statusBarColor
+    SystemUiOverlayStyle uiOverlayStyle = SystemUiOverlayStyle(
+      systemNavigationBarColor: Color(0xFF000000),
+      systemNavigationBarDividerColor: null,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.dark,
+    );
+
+    SystemChrome.setSystemUIOverlayStyle(uiOverlayStyle);
+  }
+}
+
+///内容区域的控制器
+class ContentPagerController {
+  PageController _pageController;
+
+  void jumpToPage(int index) {
+    //dart 编程技巧：安全的调用
+    _pageController?.jumpToPage(index);
+  }
+}
